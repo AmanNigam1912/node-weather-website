82ad2cbf3dbe732b84cb0a4f22e032bb7560c7d6
Aman Nigam <nigam.ama@husky.neu.edu>
2019-06-29 00:33:35 -0400

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (longitude, latitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/92b8ee221bf9d4fa45e8dfdb93a4005b/' + latitude + ',' + longitude
+
+    request({ url, json:true}, (error, {body}) => {
+        if(error){
+            callback('Unable to connect to weather service!!',undefined)
+        } else if(body.error){
+            callback('Unable to find location!')
+        } else{
+            callback(undefined, 
+                body.daily.data[0].summary  + 'It is currently ' + body.currently.temperature + ' degrees outside. There is a ' + body.currently.precipProbability + ' % chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
