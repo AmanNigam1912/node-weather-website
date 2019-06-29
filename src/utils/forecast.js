139a823f9bcb8df02a5946f8c14b0f05ca1bad19
Aman Nigam <nigam.ama@husky.neu.edu>
2019-06-29 13:41:41 -0400

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (longitude, latitude, callback) => {
             callback('Unable to find location!')
         } else{
             callback(undefined, 
-                body.daily.data[0].summary  + 'It is currently ' + body.currently.temperature + ' degrees outside. There is a ' + body.currently.precipProbability + ' % chance of rain.')
+                body.daily.data[0].summary  + 'It is currently ' + body.currently.temperature + ' degrees outside. There is a ' + body.currently.precipProbability + ' % chance of rain.' + ' High Temperature today is ' + body.daily.data[0].temperatureHigh + ' degrees with a low of ' + body.daily.data[0].temperatureLow + ' degrees.')
         }
     })
 }
